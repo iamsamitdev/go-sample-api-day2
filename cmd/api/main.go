9a08feace4836789e517c92999b66ffbee5b3fb6
Samit Koyom <samitkoyom@gmail.com>
2024-07-31 01:11:06 +0700

    initial commit

---
+++ cmd/api/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+// สร้างตัวแปรกำหนด port ที่จะใช้
+const port = 8080
+
+// สร้าง application struct สำหรับเก็บ config และ database connection
+type application struct {
+	Domain string
+}
+
+func main() {
+
+	// Set application config
+	var app application
+	app.Domain = "example.com"
+
+	// Read from command line arguments
+
+	// Connect to database
+
+	// http.HandleFunc("/", Hello)
+	// http.HandleFunc("/about", About)
+
+	// Start the server
+	fmt.Println("Starting server on ", app.Domain)
+	log.Printf("Starting server on port %d", port)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
